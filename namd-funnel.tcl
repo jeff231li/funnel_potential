e7488cf279c4e60ea11ed5ace0d8edd51ac83f80
Jeffry Setiadi <47799290+jeff231li@users.noreply.github.com>
2019-04-04 17:00:21 +1100

    Update namd-funnel.tcl

--- namd-funnel.tcl
+++ namd-funnel.tcl
@@ -1,14 +1,14 @@
 tclForces on
 
-# Funnel Options
-set v_init " 3.79790  -29.53702  -10.11779"
-set v_fina "22.35501  -14.84706  -18.17005"
+# Funnel Parameters
+set v_init      "0.0 0.0 0.0"
+set v_fina      "0.0 0.0 10.0"
 set Zcc         12.0
 set Ralpha       0.6
 set Rcyl         1.0
 set Kcyl        10.0
 
-# Input Options
+# Ligand selection Options
 set pdbFile     atoms.pdb
 set pdbCol      O
 set pdbColValue 8.0
